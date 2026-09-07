ccec0ffcb44d6dbc5334f92db527e8f9911ad8af
Anilkumar Adhikari <anilkumar.adhikari01@gmail.com>
2026-09-06 20:34:31 -0400

    Bypass the optimization step

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  images: {
+    unoptimized: true, // <-- Add this to allow the default <Image> tag to work
+  },
 };
 
 export default nextConfig;
